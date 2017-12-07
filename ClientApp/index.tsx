2d3104ce03c8ab8ec20f7fd0b1805928191d4f58
Jesper Blad Jensen <deldy@deldysoft.dk>
2017-12-07 22:06:37 +0100

    Initial

---
+++ ClientApp/index.tsx
@@ -0,0 +1,11 @@
+﻿import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+// Import CSS files
+import "tachyons";
+import "./index.css";
+
+ReactDOM.render(
+    <div className="ma4 f4 black-70">Hello from React!</div>,
+    document.getElementById("react-root")
+);
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+﻿// `CheckerPlugin` is optional. Use it if you want async error reporting.
+// We need this plugin to detect a `--watch` mode. It may be removed later
+// after https://github.com/webpack/webpack/issues/3460 will be resolved.
+const { CheckerPlugin } = require('awesome-typescript-loader')
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+
+    entry: "./ClientApp/index.tsx",
+
+    output: {
+        filename: "./wwwroot/build/app.js"
+    },
+
+    // Currently we need to add '.ts' to the resolve.extensions array.
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+
+    // Source maps support ('inline-source-map' also works)
+    devtool: 'source-map',
+
+    // Add the loader for .ts files.
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            },
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            }
+        ]
+    },
+    plugins: [
+        new CheckerPlugin(),
+        new ExtractTextPlugin("./wwwroot/build/styles.css")
+    ]
+};
